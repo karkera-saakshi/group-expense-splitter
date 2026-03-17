4e50d7117a8f1680f751021e08f791755c461f23
karkera-saakshi <saakshikarkera81@gmail.com>
2026-03-17 11:09:04 +0530

    Added JWT filter and frontend token integration

---
+++ src/main/java/com/saakshi/expense_calculator/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.saakshi.expense_calculator.config;
+
+import com.saakshi.expense_calculator.utils.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/login","/register","/login.html","/signup.html","/*.html", "/*.js", "/*.css", "/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form -> form.disable());
+        return http.build();
+    }
+}
