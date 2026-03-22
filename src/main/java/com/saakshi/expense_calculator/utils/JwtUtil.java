195664af06b32e58f55d3660ba1b0d3fc61f67a3
karkera-saakshi <saakshikarkera81@gmail.com>
2026-03-22 10:57:45 +0530

    debugging security

--- src/main/java/com/saakshi/expense_calculator/utils/JwtUtil.java
+++ src/main/java/com/saakshi/expense_calculator/utils/JwtUtil.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -14,31 +13,29 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final long EXPIRATION_TIME = 86400000;
+    private final long EXPIRATION_TIME = 86400000; // 24 hours
 
     @Value("${JWT_SECRET}")
     private String jwtSecret;
+
     private SecretKey getSigningKey() {
         byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // Generate token
     public String generateToken(String username) {
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(getSigningKey()) //
+                .signWith(getSigningKey())
                 .compact();
     }
 
-    // Extract username from token
     public String extractUsername(String token) {
         return getClaims(token).getSubject();
     }
 
-    // Validate Token
     public boolean validateToken(String token, String username) {
         return extractUsername(token).equals(username) && !isTokenExpired(token);
     }
